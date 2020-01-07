b2cf967ca421352040e35f817bbb5f950f33f099
Puttawee Saesim <sdfirst1938@gmail.com>
2020-01-07 13:08:24 +0700

    .

--- main/app.js
+++ main/app.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 
 app.get("/some", (req, res) => {
-    res.send("hello from node123!");
+    res.send("hello from node eiei!");
 });
 
 // SECTION  Database
