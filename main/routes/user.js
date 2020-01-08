5576511595cc7949c3d59d6c1088866199247e7b
Macintosh HD <>
2020-01-08 14:55:11 +0700

    .

--- main/routes/user.js
+++ main/routes/user.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { sayHi } = require('../controllers/user');
-const { sayHello } = require('../controller/user');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
-router.get('/signin', sayHi);
+const { userById, read, update, purchaseHistory } = require('../controllers/user');
+
+router.get('/secret', requireSignin, (req, res) => {
+    res.json({
+        user: 'got here yay'
+    });
+});
+
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
+
+router.param('userId', userById);
 
 module.exports = router;
