ac091e043495695cf934943e5f2fb3b9b4d3caf7
Macintosh HD <>
2020-02-23 17:01:39 +0700

    complete create order on Database

--- front-end/src/core/Checkout.js
+++ front-end/src/core/Checkout.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getProducts, getBraintreeClientToken } from './apiCore';
+import { getProducts, getBraintreeClientToken, processPayment, createOrder } from './apiCore';
 import { emptyCart } from './cartHelpers';
 import Card from './Card';
 import { Link } from 'react-router-dom';
@@ -7,11 +7,11 @@ import {isAuthenticated} from '../auth';
 import DropIn from 'braintree-web-drop-in-react'
 
 
-// import "braintree-web"; // not using this package
 
 // SECTION state to handle braintree
-const Checkout = ({products}) => {
+const Checkout = ({products, setRun = f => f, run = undefined }) => {
     const [data, setData] = useState({
+        loading: false,
         success: false, 
         clientToken: null, 
         error: '', 
@@ -30,7 +30,7 @@ const getToken = (userId, token) => {
             setData({...data, error: data.error})
 
         }else{
-            setData({...data, clientToken:data.clientToken})
+            setData({ clientToken:data.clientToken})
         }
     })
 }
@@ -42,6 +42,7 @@ useEffect(() => {
 },[])
 
 const buy = () => {
+    // setData({loading:true});
     // send the nonce to your server
     // nonce = data.instance.requestPaymentMethod()
     let nonce;
@@ -56,13 +57,61 @@ const buy = () => {
         //     nonce,
         //     getTotal(products)
         // );
+        const paymentData = {
+            paymentMethodNonce: nonce, 
+            amount: getTotal(products)
+        }
+        // NOTE Sennd these data to backend (To braintree server)
+     
+        processPayment(userId, token, paymentData)
+        .then(response => { //NOTE this is response from payment (braintree)
+            console.log(response); // SEE THE RESPONSE TRANSACTION_ID ETC
+
+
+            // SECTION  create order
+            const createOrderData ={ //NOTE keep it as Object before storing in Datase
+                products: products,
+                transaction_id: response.transaction.id, // NOTE return from braintree
+                amount: response.transaction.amount, //NOTE  how much user pay for this order (return from braintree)
+                address: data.address
+            }
+            //NOTE Then put it into database via createOrder method
+            createOrder(userId,token,createOrderData)     
+            setData({...data, success: response.success})
+            //NOTE After finished create order, remove every thing on cart page by default
+            emptyCart(()=>{
+                setRun(!run);                
+                setData({
+                    loading: false,
+                    success: true
+                });
+            })
+
+            // NOTE emty cart
+
+          
+
+                    
+           } )
+        .catch(error => {console.log(error)
+        setData({loading:false})
+        }
+        );
     })
+
+
         .catch(error =>{
             console.log(`dropin error:`,error)
             setData({...data, error: error.message})
         })
 }
 
+//SECTION Address handle
+
+// NOTE grab address from a user inputs
+const handleAddress = event => {
+    setData({ ...data, address: event.target.value });
+};
 
 
 const showDropIn = () => (
@@ -73,13 +122,26 @@ const showDropIn = () => (
             
             // https://www.npmjs.com/package/braintree-web-drop-in-react 
             <div>
+                  <div className="gorm-group mb-3">
+                        <label className="text-muted">Delivery address:</label>
+                        <textarea
+                            onChange={handleAddress}
+                            className="form-control"
+                            value={data.address}
+                            placeholder="Type your delivery address here..."
+                        />
+                    </div>
+
                 <DropIn
                         options={{
                             authorization: data.clientToken,
+                            paypal: {
+                                flow: 'vault'
+                            }
                         }}
                         onInstance={instance => (data.instance = instance)}
                     />
-                  <button onClick={buy} className="btn btn-danger">Pay</button>
+                  <button onClick={buy} className="btn btn-danger btn-block">Pay</button>
             </div>
 
         ): null}
@@ -116,7 +178,7 @@ const showLoading = loading => loading && <h2 className="text-danger">Loading...
     return(
         <div>
             {showError(data.error)}
-            {/* {showSuccess(data.success)} */}
+            {showSuccess(data.success)}
             {showLoading(data.loading)}
             <h2>Total: ${getTotal()}</h2>
 
